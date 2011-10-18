f62c387e3bfcb85527ac6efa5bd7c1ae144264a4
David Ceresuela <davidzgz123@gmail.com>
2011-10-18 16:31:46 +0200

    Added scripts to start and stop virtual machines

---
+++ dev/vm/iniciar.sh
@@ -0,0 +1,4 @@
+sudo virsh start karmic-6GB
+sudo virsh start karmic-6GB-2
+sudo virsh start karmic-6GB-3
+sudo virsh start karmic-6GB-4
---
+++ dev/vm/terminar.sh
@@ -0,0 +1,4 @@
+sudo virsh shutdown karmic-6GB
+sudo virsh shutdown karmic-6GB-2
+sudo virsh shutdown karmic-6GB-3
+sudo virsh shutdown karmic-6GB-4
