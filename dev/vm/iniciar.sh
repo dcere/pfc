f5fda9468e9e3e20f37f55a964f3e6852e956511
David Ceresuela <davidzgz123@gmail.com>
2011-10-24 11:51:47 +0200

    Updated shell scripts iniciar.sh and terminar.sh to reflect the changes on domain names

--- dev/vm/iniciar.sh
+++ dev/vm/iniciar.sh
@@ -1,4 +1,4 @@
-sudo virsh start karmic-6GB
+sudo virsh start karmic-6GB-1
 sudo virsh start karmic-6GB-2
 sudo virsh start karmic-6GB-3
 sudo virsh start karmic-6GB-4
--- dev/vm/terminar.sh
+++ dev/vm/terminar.sh
@@ -1,4 +1,4 @@
-sudo virsh shutdown karmic-6GB
+sudo virsh shutdown karmic-6GB-1
 sudo virsh shutdown karmic-6GB-2
 sudo virsh shutdown karmic-6GB-3
 sudo virsh shutdown karmic-6GB-4
