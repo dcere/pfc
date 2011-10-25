4024734325ec292a6bde9975b0ee08540f0dbf5a
David Ceresuela <davidzgz123@gmail.com>
2011-10-26 01:27:53 +0200

    Added some host network configuration files

--- dev/vm/conectar-gnome.sh
+++ dev/vm/conectar-gnome.sh
@@ -1,4 +1,21 @@
-gnome-terminal --tab -e 'ssh root@192.168.1.101'\
-               --tab -e 'ssh root@192.168.1.102'\
-               --tab -e 'ssh root@192.168.1.103'\
-               --tab -e 'ssh root@192.168.1.104'
+#gnome-terminal --tab -e 'ssh root@192.168.1.101'\
+#               --tab -e 'ssh root@192.168.1.102'\
+#               --tab -e 'ssh root@192.168.1.103'\
+#               --tab -e 'ssh root@192.168.1.104'
+
+COMMAND="gnome-terminal"
+LIST=" "
+
+if [ $# -eq 0 ]
+then
+  echo "$0 : Introduce la cantidad de máquinas a las que te quieres conectar"
+  exit 1
+fi
+
+for ((  i = 1 ;  i <= $1;  i++  ))
+do
+  LIST="$LIST"" --tab -e ""'""ssh root@192.168.1.10$i""'"
+done
+
+COMMAND="$COMMAND""$LIST"
+echo $COMMAND
