f47424d86974088514bc5baff1ff30fc192d21ea
David Ceresuela <davidzgz123@gmail.com>
2012-01-13 02:05:24 +0100

    [dev] Added management script + [adm] Worksheet update

---
+++ dev/puppet/faces/clean.sh
@@ -0,0 +1,19 @@
+# Description:
+#   Cleans the directory
+#
+# Synopsis:
+#   clean.sh
+#
+# Arguments:
+#   - None
+#
+# Examples:
+#   _$: clean.sh
+#
+#
+# Author:
+#   David Ceresuela
+
+# Clean
+rm image-?.img
+rm *.xml
