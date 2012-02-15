cd54f390cce0bfb5d3770752d4dd96ca4a80713f
David Ceresuela <davidzgz123@gmail.com>
2012-02-15 20:01:48 +0100

    [dev] Cloud installation script slightly improved

--- dev/puppet/cloud-module/install.sh
+++ dev/puppet/cloud-module/install.sh
@@ -15,23 +15,24 @@
 #   David Ceresuela
 
 NAME="cloud"
+PUPPET_DST="/etc/puppet/modules"
 
 # Create directories
-sudo mkdir -p /etc/puppet/modules/$NAME/{files,templates,manifests}
-sudo mkdir -p /etc/puppet/modules/$NAME/lib/puppet/type
-sudo mkdir -p /etc/puppet/modules/$NAME/lib/puppet/provider/$NAME
+sudo mkdir -p $PUPPET_DST/$NAME/{files,templates,manifests}
+sudo mkdir -p $PUPPET_DST/$NAME/lib/puppet/type
+sudo mkdir -p $PUPPET_DST/$NAME/lib/puppet/provider/$NAME
 
 # Copy manifest
-sudo cp ./manifests/init.pp /etc/puppet/modules/$NAME/manifests/init.pp
+sudo cp ./manifests/init.pp $PUPPET_DST/$NAME/manifests/init.pp
 
 # Copy type and provider
 TYPE_SRC="./lib/puppet/type"
-TYPE_DST="/etc/puppet/modules/$NAME/lib/puppet/type"
+TYPE_DST="$PUPPET_DST/$NAME/lib/puppet/type"
 PROVIDER_SRC="./lib/puppet/provider"
-PROVIDER_DST="/etc/puppet/modules/$NAME/lib/puppet/provider/$NAME"
+PROVIDER_DST="$PUPPET_DST/$NAME/lib/puppet/provider/$NAME"
+
 sudo cp $TYPE_SRC/cloud.rb $TYPE_DST/cloud.rb
 sudo cp $PROVIDER_SRC/$NAME/cloudp.rb $PROVIDER_DST/cloudp.rb
 
 # Copy test
-PUPPET_DST="/etc/puppet/"
-sudo cp ./test.rb $PUPPET_DST/modules/$NAME
+sudo cp ./test.rb $PUPPET_DST/$NAME
