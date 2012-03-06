9bcc2b2db6e2ffe68ae1d2b542968417649a732b
David Ceresuela <davidzgz123@gmail.com>
2012-03-06 11:23:13 +0100

    [dev] Puppet module installation lab script modified to work with ssh and scp

--- dev/puppet/cloud-module/install-lab.sh
+++ dev/puppet/cloud-module/install-lab.sh
@@ -2,13 +2,13 @@
 #   Installs the cloud puppet module
 #
 # Synopsis:
-#   install-lab.sh
+#   install-lab.sh <IP>
 #
 # Arguments:
-#   - None.
+#   - IP address: IP address of remote host.
 #
 # Examples:
-#   _$: install-lab.sh
+#   _$: install-lab.sh 155.210.155.170
 #
 #
 # Author:
@@ -17,15 +17,17 @@
 NAME="cloud"
 PUPPET_DST="/etc/puppet/modules"
 
+SSH="ssh root@$1"
+
 # Create directories
-mkdir -p $PUPPET_DST/$NAME/{files,templates,manifests}
-mkdir -p $PUPPET_DST/$NAME/lib/puppet/type
-mkdir -p $PUPPET_DST/$NAME/lib/puppet/provider/$NAME
+$SSH 'mkdir -p $PUPPET_DST/$NAME/{files,templates,manifests}'
+$SSH 'mkdir -p $PUPPET_DST/$NAME/lib/puppet/type'
+$SSH 'mkdir -p $PUPPET_DST/$NAME/lib/puppet/provider/$NAME'
 
 # Copy manifests
-cp ./manifests/init.pp $PUPPET_DST/$NAME/manifests/init.pp
-cp ./manifests/term.pp $PUPPET_DST/$NAME/manifests/term.pp
-cp ./manifests/test.pp $PUPPET_DST/$NAME/manifests/test.pp
+scp ./manifests/init.pp root@$1:$PUPPET_DST/$NAME/manifests/init.pp
+scp ./manifests/term.pp root@$1:$PUPPET_DST/$NAME/manifests/term.pp
+scp ./manifests/test.pp root@$1:$PUPPET_DST/$NAME/manifests/test.pp
 
 # Copy type and provider
 TYPE_SRC="./lib/puppet/type"
@@ -33,11 +35,11 @@ TYPE_DST="$PUPPET_DST/$NAME/lib/puppet/type"
 PROVIDER_SRC="./lib/puppet/provider"
 PROVIDER_DST="$PUPPET_DST/$NAME/lib/puppet/provider/$NAME"
 
-cp $TYPE_SRC/cloud.rb $TYPE_DST/cloud.rb
-cp $PROVIDER_SRC/$NAME/cloudp.rb $PROVIDER_DST/cloudp.rb
+scp $TYPE_SRC/cloud.rb            root@$1:$TYPE_DST/cloud.rb
+scp $PROVIDER_SRC/$NAME/cloudp.rb root@$1:$PROVIDER_DST/cloudp.rb
 
 # Copy test
-cp ./test.rb $PUPPET_DST/$NAME
+scp ./test.rb root@$1:$PUPPET_DST/$NAME
 
 # Copy validation script
-cp ./validate.sh $PUPPET_DST/$NAME
+scp ./validate.sh root@$1:$PUPPET_DST/$NAME
