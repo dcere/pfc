7ad4e77a992604e42b2d512ac0e4a51704bee4be
David Ceresuela <davidzgz123@gmail.com>
2012-03-13 15:31:18 +0100

    [dev] Added RPC Agent and Client (for test purposes)

---
+++ dev/mcollective/helloworld-client.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+require 'mcollective'
+include MCollective::RPC
+mc = rpcclient("helloworld")
+printrpc mc.echo(:msg => "Welcome to MCollective Simple RPC")
+printrpcstats
+mc.disconnect
---
+++ dev/mcollective/helloworld.rb
@@ -0,0 +1,11 @@
+module MCollective
+   module Agent
+      class Helloworld<RPC::Agent
+      # Basic echo server
+         def echo_action
+            validate :msg, String
+            reply.data = request[:msg]
+         end
+      end
+   end
+end
