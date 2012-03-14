0eea55b4b8b6a09ba0ab08364c2492ef7f42b2bc
David Ceresuela <davidzgz123@gmail.com>
2012-03-14 12:05:13 +0100

    [dev] Added puppet installation script

---
+++ dev/puppet-installation/puppet-installation.sh
@@ -0,0 +1,35 @@
+# Description:
+#   Creates the proper environment before installing puppet
+#
+# Synopsis:
+#   puppet-installation-environment.sh
+#
+# Arguments:
+#   - None
+#
+# Examples:
+#   _$: . ./puppet-installation-environment.sh
+#
+#
+# Author:
+#   David Ceresuela
+
+PUPPET_STABLE="puppet-2.7.9"
+FACTER_STABLE="facter-1.6.4"
+
+./puppet-installation-environment.sh
+
+tar xf "$FACTER_STABLE.tar.gz"
+rm "$FACTER_STABLE.tar.gz"
+cd ./$FACTER_STABLE
+./install.rb
+cd ../
+
+tar xf "$PUPPET_STABLE.tar.gz"
+rm "$PUPPET_STABLE.tar.gz"
+cd ./$PUPPET_STABLE
+./install.rb
+cd ../
+
+# Mini test
+facter
--- dev/vm/copy-puppet-installation-files.sh
+++ dev/vm/copy-puppet-installation-files.sh
@@ -30,12 +30,14 @@ RUBY_STABLE="ruby-1.9.3-p0.tar.gz"
 PUPPET_STABLE="puppet-2.7.9.tar.gz"
 FACTER_STABLE="facter-1.6.4.tar.gz"
 PUPPET_ENVIRONMENT="puppet-installation-environment.sh"
+PUPPET_INSTALL="puppet-installation.sh"
 
 DESTINATION_PATH="root@$1:/root/."
 
 # Check if they already exist?
 # if test -f ...
-#scp ../ruby-installation/$RUBY_STABLE        $DESTINATION_PATH
-scp ../puppet-installation/$PUPPET_STABLE    $DESTINATION_PATH
-scp ../puppet-installation/$FACTER_STABLE    $DESTINATION_PATH
-scp ./$PUPPET_ENVIRONMENT                    $DESTINATION_PATH
+#scp ../ruby-installation/$RUBY_STABLE            $DESTINATION_PATH
+scp ../puppet-installation/$PUPPET_STABLE        $DESTINATION_PATH
+scp ../puppet-installation/$FACTER_STABLE        $DESTINATION_PATH
+scp ../puppet-installation/$PUPPET_INSTALL       $DESTINATION_PATH
+scp ../puppet-installation/$PUPPET_ENVIRONMENT   $DESTINATION_PATH
