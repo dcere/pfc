e148e1983a6ce2becef67198aac6560aeb0c7412
David Ceresuela <davidzgz123@gmail.com>
2012-05-08 15:20:42 +0200

    [dev] Make tests in test_leader* files as function to allow puppet describe -s

--- dev/puppet/cloud-module/lib/puppet/provider/cloud/test_leader2.rb
+++ dev/puppet/cloud-module/lib/puppet/provider/cloud/test_leader2.rb
@@ -1,26 +1,28 @@
 require 'mcollective'
 include MCollective::RPC
 
-puts "Creatig client"
-mc = rpcclient("leader")
+def test
+   puts "Creatig client"
+   mc = rpcclient("leader")
 
-puts "Retrieving nodes ids"
-printrpc mc.ask_id()
-printrpcstats
-puts "Disconnecting"
+   puts "Retrieving nodes ids"
+   printrpc mc.ask_id()
+   printrpcstats
+   puts "Disconnecting"
 
-id = "123"
+   id = "123"
 
-puts "Sending new leader information"
-out2 = Helpers.rpcresults mc.new_leader(:id => id)
-out3 = mc.new_leader(:id => id)
-puts "Disconnecting"
-mc.disconnect
+   puts "Sending new leader information"
+   out2 = Helpers.rpcresults mc.new_leader(:id => id)
+   out3 = mc.new_leader(:id => id)
+   puts "Disconnecting"
+   mc.disconnect
 
-puts out3[0].agent
-puts out3[0].results[:sender]
-puts out3[1].results[:sender]
+   puts out3[0].agent
+   puts out3[0].results[:sender]
+   puts out3[1].results[:sender]
 
-puts "Output 2:"
-puts "---------"
-puts out2
+   puts "Output 2:"
+   puts "---------"
+   puts out2
+end
