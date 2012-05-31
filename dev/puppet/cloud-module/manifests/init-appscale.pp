0bf10260ce995f02df8d7e565fe82647259499b8
David Ceresuela <davidzgz123@gmail.com>
2012-05-31 17:30:16 +0200

    [dev] AppScale cloud with 1 master and 2 open nodes

--- dev/puppet/cloud-module/manifests/init-appscale.pp
+++ dev/puppet/cloud-module/manifests/init-appscale.pp
@@ -1,7 +1,7 @@
 cloud {'mycloud':
    type     => "appscale",
-   ip_file  => "/etc/puppet/modules/cloud/files/appscale-simple-ip.yaml",
-   img_file => "/etc/puppet/modules/cloud/files/appscale-simple-img.yaml",
+   ip_file  => "/etc/puppet/modules/cloud/files/appscale-ip.yaml",
+   img_file => "/etc/puppet/modules/cloud/files/appscale-img.yaml",
    domain  => "/etc/puppet/modules/cloud/files/mycloud-template.xml",
    pool    => ["155.210.155.70"],
    ensure  => running,
