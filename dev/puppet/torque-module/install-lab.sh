702f4081022d846c36be301a44f6a4ba4533f315
David Ceresuela <davidzgz123@gmail.com>
2012-06-26 13:13:34 +0200

    [dev] Installation script allows different users

--- dev/puppet/torque-module/install-lab.sh
+++ dev/puppet/torque-module/install-lab.sh
@@ -2,9 +2,10 @@
 #   Installs the torque puppet module
 #
 # Synopsis:
-#   install-lab.sh <IP> <files>
+#   install-lab.sh <user> <IP> <files>
 #
 # Arguments:
+#   - User: User of remote host.
 #   - IP address: IP address of remote host.
 #   - Files: Files to install
 #     - all:   all the files
@@ -15,21 +16,21 @@
 #     - trq:   Torque local manifests and monitor files
 #
 # Examples:
-#   _$: install-lab.sh 155.210.155.170 all
-#   _$: install-lab.sh 155.210.155.170 tp
-#   _$: install-lab.sh 155.210.155.170 man
-#   _$: install-lab.sh 155.210.155.170 files
+#   _$: install-lab.sh root 155.210.155.170 all
+#   _$: install-lab.sh root 155.210.155.170 tp
+#   _$: install-lab.sh root 155.210.155.170 man
+#   _$: install-lab.sh root 155.210.155.170 files
 #
 #
 # Author:
 #   David Ceresuela
 
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then
-   echo "Use: $0 <IP address> <set of files>"
+   echo "Use: $0 <user> <IP address> <set of files>"
    echo "Examples
-$0 155.210.155.170 all
-$0 155.210.155.170 tp
+$0 root  155.210.155.170 all
+$0 david 155.210.155.170 tp
 "
    exit 1
 fi
@@ -37,10 +38,10 @@ fi
 NAME="torque"
 PUPPET_DST="/etc/puppet/modules"
 
-SSH="ssh root@$1"
+SSH="ssh $1@$2"
 
 # Create directories
-if [ $2 != "trq" ]
+if [ $3 != "trq" ]
 then
    $SSH mkdir -p $PUPPET_DST/$NAME/{files,templates,manifests}
    $SSH mkdir -p $PUPPET_DST/$NAME/lib/puppet/type
@@ -49,9 +50,9 @@ then
 fi
 
 # Copy manifests
-if [ $2 = "man" -o $2 = "all" ]
+if [ $3 = "man" -o $3 = "all" ]
 then
-   scp ./manifests/*    root@$1:$PUPPET_DST/$NAME/manifests/
+   scp ./manifests/*    $1@$2:$PUPPET_DST/$NAME/manifests/
 fi
 
 # Copy type and provider
@@ -60,42 +61,48 @@ TYPE_DST="$PUPPET_DST/$NAME/lib/puppet/type"
 PROVIDER_SRC="./lib/puppet/provider"
 PROVIDER_DST="$PUPPET_DST/$NAME/lib/puppet/provider/$NAME"
 
-if [ $2 = "tp" -o $2 = "all" ]
+if [ $3 = "tp" -o $3 = "all" ]
 then
-   scp $TYPE_SRC/torque.rb          root@$1:$TYPE_DST/torque.rb
+   scp $TYPE_SRC/torque.rb          $1@$2:$TYPE_DST/torque.rb
 
    # All provider files
-   scp -r $PROVIDER_SRC/$NAME/*     root@$1:$PROVIDER_DST/
+   scp -r $PROVIDER_SRC/$NAME/*     $1@$2:$PROVIDER_DST/
 fi
 
 # Copy type and provider files directory
-if [ $2 = "files" -o $2 = "all" ]
+if [ $3 = "files" -o $3 = "all" ]
 then
-   scp -r ./files/*     root@$1:$PUPPET_DST/$NAME/files/
+   scp -r ./files/*     $1@$2:$PUPPET_DST/$NAME/files/
 fi
 
 # Copy test
-if [ $2 = "test" -o $2 = "all" ]
+if [ $3 = "test" -o $3 = "all" ]
 then
-   scp ./test.rb     root@$1:$PUPPET_DST/$NAME
+   scp ./test.rb     $1@$2:$PUPPET_DST/$NAME
 fi
 
 # Copy validation script
-if [ $2 = "test" -o $2 = "all" ]
+if [ $3 = "test" -o $3 = "all" ]
 then
-   scp ./validate.sh    root@$1:$PUPPET_DST/$NAME
+   scp ./validate.sh    $1@$2:$PUPPET_DST/$NAME
 fi
 
 # Copy Torque monitor files ans start scripts
-TORQUE_DST="/root/cloud/torque"
-if [ $2 = "trq" -o $2 = "all" ]
+if [ $1 = "root" ]
+then
+   TORQUE_DST="/$1/cloud/torque"
+else
+   TORQUE_DST="/home/$1/cloud/torque"
+fi
+
+if [ $3 = "trq" -o $3 = "all" ]
 then
    $SSH mkdir -p $PUPPET_DST/$NAME/files/torque-god
-   scp ./files/torque-god/*      root@$1:$PUPPET_DST/$NAME/files/torque-god/
+   scp ./files/torque-god/*      $1@$2:$PUPPET_DST/$NAME/files/torque-god/
    
    $SSH mkdir -p $PUPPET_DST/$NAME/files/torque-start
-   scp ./files/torque-start/*    root@$1:$PUPPET_DST/$NAME/files/torque-start/
+   scp ./files/torque-start/*    $1@$2:$PUPPET_DST/$NAME/files/torque-start/
    
    $SSH mkdir -p $TORQUE_DST
-   scp ./files/torque-start/*    root@$1:$TORQUE_DST/
+   scp ./files/torque-start/*    $1@$2:$TORQUE_DST/
 fi
