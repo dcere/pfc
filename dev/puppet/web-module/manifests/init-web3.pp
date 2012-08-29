e681df81b914a2282d7b32d9542a0947a2de21fa
David Ceresuela <davidzgz123@gmail.com>
2012-08-29 19:48:56 +0200

    [dev] Added {init,stop}-web3 manifests to test several physical machines

--- dev/puppet/web-module/manifests/init-web3.pp
+++ dev/puppet/web-module/manifests/init-web3.pp
@@ -3,6 +3,6 @@ web {'mycloud':
    server    => ["/etc/puppet/modules/web/files/server-ips.txt", "/etc/puppet/modules/web/files/server-imgs.txt"],
    database  => ["155.210.155.177", "/var/tmp/dceresuela/lucid-db.img"],
    vm_domain => "/etc/puppet/modules/web/files/mycloud-template.xml",
-   pool      => ["155.210.155.70"],
+   pool      => ["155.210.155.70", "155.210.155.77"],
    ensure    => running,
 }
---
+++ dev/puppet/web-module/manifests/stop-web3.pp
@@ -0,0 +1,7 @@
+web {'mycloud':
+   balancer => ["155.210.155.175", "/var/tmp/dceresuela/lucid-lb.img"],
+   server   => ["/etc/puppet/modules/web/files/server-ips.txt", "/etc/puppet/modules/web/files/server-imgs.txt"],
+   database => ["155.210.155.177", "/var/tmp/dceresuela/lucid-db.img"],
+   pool     => ["155.210.155.70", "155.210.155.77"],
+   ensure   => stopped,
+}
